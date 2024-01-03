1ec3dbfe63137405e52d09e1f91cf9d71c73edb3
Piusbassey <okonpius5@gmail.com>
2024-01-03 11:55:22 +0000

    create_a_new_sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-words = str.split(',')[2].split()[3:7]
-result = ' '.join(words)
-print(result + '\n')
+print(' '.join(str.split(',')[2].split()[3:7]) + '\n', end='')
