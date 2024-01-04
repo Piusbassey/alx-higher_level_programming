2ef1e872d9f84ee902822c0e0e22347b29c15d3f
Piusbassey <okonpius5@gmail.com>
2024-01-04 12:17:05 +0000

    to_uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env/python3
+
+def uppercase(str):
+    for char in str;
+
+    if 'a' <= char <= 'z':
+        print(chr(ord(char) - 32), end='')
+    else:
+        print(char, end='')
+    print()
+
+    uppercase("best")
+    uppercase("Best School 98 Battery street")
