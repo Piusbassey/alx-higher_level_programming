6de89fac82f035c3b71120ee89dc05660f5343b4
Piusbassey <okonpius5@gmail.com>
2024-01-09 14:00:50 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,13 @@ import sys
 
 arguments = sys.argv
 
-num_arguments = len(arguments)
+num_arguments = len(arguments) - 1
 
 if num_arguments == 0:
     print("0 arguments.")
 else:
     print(f"{num_arguments} argument{'s' if num_arguments != 1 else ''}:")
+    argument_number = 1
     for argument in arguments[1:]:
-        print(f"1: {argument}")
+        print(f"{argument_number}: {argument:}")
+        argument_number += 1
