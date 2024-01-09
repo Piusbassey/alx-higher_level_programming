df9916b7c98f97f5ddf2ff5852fa6cd08d0863e1
Piusbassey <okonpius5@gmail.com>
2024-01-09 07:39:46 -0800

    calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
 
-print(add(a, b))
-print(sub(a, b))
-print(mul(a, b))
-print(mul(a, b))
+print(f"{a} + {b} = {add(a, b)}")
+print(f"{a} - {b} = {sub(a, b)}")
+print(f"{a} * {b} = {mul(a, b)}")
+print(f"{a} / {b} = {div(a, b)}")
