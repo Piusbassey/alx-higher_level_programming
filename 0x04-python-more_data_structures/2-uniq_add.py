cd72fb2b9708bd9d21afb724c43d48c02647ae80
Piusbassey <okonpius5@gmail.com>
2024-01-11 14:01:06 +0000

    unique_addition

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_sum = sum(set(my_list))
+    return unique_sum
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 1, 4, 2, 5]
+    resut = unique_add(my_list)
+    print("Result: {:d}".format(result))
