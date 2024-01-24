ec5286b342e744021775b0051757f52f0eabe017
Piusbassey <okonpius5@gmail.com>
2024-01-24 12:02:25 +0000

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module containing a class Square that defines a square.
+"""
+
+class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+    def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
