b69d4e7cdb1fc86d40bf187480084c82f7657f3b
Piusbassey <okonpius5@gmail.com>
2024-01-30 09:58:09 +0000

    divide_a_matrix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python3
-python3 -c 'print(__import__("0-add_integer").add_integer.__doc__)'
+"""
+This module contains a function that adds 2 integers.
+"""
+
+
 def add_integer(a, b=98):
     """
-    Adds two numbers and returns an integer
+    This function adds 2 integers and returns the result.
+
+    Args:
+        a (int or float): The first number to be added.
+        b (int or float, optional): The second number to be added. Defaults to 98.
+
+    Returns:
+        int: The sum of a and b.
+
+    Raises:
+        TypeError: If a or b is not an integer or a float.
     """
-    if not (isinstance(a, int, float)):
+    # Check if a and b are integers or floats
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not (isinstance(b, int, float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
+    # Cast a and b to integers if they are floats
     a = int(a)
     b = int(b)
 
+    # Return the sum of a and b
     return a + b
