fc19835dc6d562d32623e9580334dadfdf128126
Piusbassey <okonpius5@gmail.com>
2024-01-31 13:35:23 +0000

    low_memory_cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 class LockedClass:
-    __slots__ = ["first_name"]
-
-    def __init__(self):
-        pass
+    def __setattr__(self, attr, value):
+        if not hasattr(self, 'first_name') and attr != 'first_name':
+            raise AttributeError("'LockedClass' object has no attribute '{}'".format(attr))
+        super().__setattr__(attr, value)
