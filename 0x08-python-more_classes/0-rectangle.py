51fa8e71fad73cfd1840f78cbb430e7befbb03e9
Piusbassey <okonpius5@gmail.com>
2024-01-31 20:48:45 +0000

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Rectangle:
+    """
+    Empty class representing a rectangle.
+    This class does not have any attributes or methods defined.
+    It serves as a placeholder for defining properties and behaviors
+    of a rectangle in future implementations.
+    """
+    pass
