ecfef8786a015abc4f708b2c4dc9903ed6f6a233
Piusbassey <okonpius5@gmail.com>
2024-02-05 12:59:17 +0000

    can_i

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python3
-def add_attribute(obj, attribute, value):
+def add_attribute(obj, attr_name, attr_value):
     """
-    Adds a new attribute to an object if it's possible.
-
-    Raises a TypeError exception if the object can't have a new attribute.
+    Adds a new attribute to an object if possible.
 
     Args:
-        obj: The object to add the attribute to.
-        attribute (str): The name of the attribute.
-        value: The value of the attribute.
+        obj: The object to which the attribute should be added.
+        attr_name: The name of the attribute.
+        attr_value: The value to assign to the attribute.
 
     Raises:
-        TypeError: If the object can't have a new attribute.
+        TypeError: If the object cannot have a new attribute.
     """
-    if not hasattr(obj, "__dict__"):
+    if not hasattr(obj, '__dict__'):
         raise TypeError("can't add new attribute")
-    setattr(obj, attribute, value)
+
+    setattr(obj, attr_name, attr_value)
+
+# Example usage
+class MyClass():
+    pass
+
+mc = MyClass()
+add_attribute(mc, "name", "John")
+print(mc.name)
+
+try:
+    a = "My String"
+    add_attribute(a, "name", "Bob")
+    print(a.name)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
