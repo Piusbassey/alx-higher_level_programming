63529bc6d7a7a91c396491838bd7d6f428a4a53a
Piusbassey <okonpius5@gmail.com>
2024-02-05 11:46:30 +0000

    my_integer

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -2,14 +2,17 @@
 class MyInt(int):
     """
     A class representing a rebel integer.
+
     Inherits from the int class.
     """
+
     def __eq__(self, other):
         """
-        Overrides the equality operator (==) to return True if the values
-        are not equal.
+        Overrides the equality operator (==) to return True if the values are not equal.
+
         Args:
             other: The value to compare with.
+
         Returns:
             bool: True if the values are not equal; False otherwise.
         """
@@ -17,10 +20,11 @@ class MyInt(int):
 
     def __ne__(self, other):
         """
-        Overrides the inequality operator (!=) to return True
-        if the values are equal.
+        Overrides the inequality operator (!=) to return True if the values are equal.
+
         Args:
             other: The value to compare with.
+
         Returns:
             bool: True if the values are equal; False otherwise.
         """
